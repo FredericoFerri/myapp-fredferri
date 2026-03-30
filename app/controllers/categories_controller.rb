f5c2d556f8b1bc1f687f970d5635441ca12cc019
Frederico Ferri <ferrifrederico040@gmail.com>
2026-03-30 21:38:24 +0100

    TASK 3 - Worksheet1 - Creation of controllers (edit, view, delete) for Category class - Creation of views for Category class (edit, view) - Redirection to Category form page when adding or editing a category - Inclusion of database validation for Category class (verification to avoid categories with duplicate codes and/or names) - Homepage created with cards for both form pages (Category and Product)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,60 @@
 class CategoriesController < ApplicationController
-    # GET /categories/new
-    def new
-        @category = Category.new
-    end
+    before_action :set_category, only: %i[ show edit update destroy ]
 
-    # POST /categories
-    def create
-        category_params = params.require(:category).permit(:code, :name)
-        @category = Category.new(category_params)
-
-        if @category.save
-            redirect_to new_category_path, notice: 'Category was created.'
-        else
-            render :new
+    public
+        # GET /categories
+        def index
+            @categories = Category.all
         end
-    end
 
+        # GET /categories/1
+        def show
+        end
+
+        # GET /categories/new
+        def new
+            @category = Category.new
+        end
+
+        # GET /categories/1/edit
+        def edit 
+        end 
+
+        # POST /categories
+        def create
+            category_params = params.require(:category).permit(:code, :name)
+            @category = Category.new(category_params)
+
+            if @category.save
+                redirect_to new_category_path, notice: 'Category was created.'
+            else
+                render :new
+            end
+        end
+
+        # PATCH/PUT /categories/1
+        def update
+            if @category.update(category_params)
+                redirect_to categories_path, notice: "category was successfully updated."
+            else
+                render :edit, status: :unprocessable_entity
+            end
+        end
+
+        # DELETE /categories/1
+        def destroy
+            @category.destroy!
+            redirect_to categories_path, notice: "category was successfully destroyed.", status: :see_other
+        end
+
+    private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_category
+            @category = Category.find(params.expect(:id))
+        end
+
+        # Only allow a list of trusted parameters through.
+        def category_params
+        params.expect(category: [ :name, :description, :cost ])
+        end
 end
